98111a35798d6955b365501a8828b07c63613b52
Luiz Almeida (lhas) <luizhrqas@gmail.com>
2021-08-23 10:14:33 -0300

    update text

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to Underdev
+          Welcome to Underdev!
         </h1>
 
         <p className={styles.description}>
