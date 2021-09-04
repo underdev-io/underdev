f84d26b15c1c9d6acdcf10a9c1cfdb3bd020bf1b
Luiz Almeida (lhas) <luizhrqas@gmail.com>
2021-09-04 12:52:49 -0300

    initialize testimonials list

--- components/Testimonials/index.tsx
+++ components/Testimonials/index.tsx
@@ -1,3 +1,4 @@
+import { MdKeyboardArrowLeft, MdKeyboardArrowRight } from "react-icons/md";
 import styled from "styled-components";
 import { HeroUnit } from "../HeroUnit";
 import { Wrapper as HeroUnitWrapper } from "../HeroUnit";
@@ -11,7 +12,7 @@ const Wrapper = styled.section`
   min-height: 100vh;
   flex-direction: column;
   align-items: center;
-  color: #fff;
+  color: #00e9c5;
 
   ${LogoWrapper} {
     display: block;
@@ -23,7 +24,7 @@ const Wrapper = styled.section`
   ${HeroUnitWrapper} {
     margin: 0 auto;
     max-width: 400px;
-    margin-top: 60px;
+    margin-top: 100px;
     margin-bottom: 20px;
     align-items: center;
 
@@ -39,26 +40,66 @@ const Wrapper = styled.section`
   }
 `;
 
+const TestimonialsListWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  column-gap: 50px;
+
+  button {
+    width: 60px;
+    height: 60px;
+    background: transparent;
+    border: none;
+    cursor: pointer;
+    color: #00e9c5;
+  }
+`;
+
 const TestimonialsList = styled.ul`
   width: 100%;
+  list-style-type: none;
+  margin-top: 60px;
 `;
 
 const TestimonialsListItem = styled.li`
   width: 100%;
+  max-width: 620px;
+
+  p {
+    font-family: "Gordita Light";
+    line-height: 1.75;
+    font-size: 21px;
+  }
+
+  strong {
+    font-family: "Gordita Bold";
+    line-height: 1.75;
+    font-size: 22px;
+    margin-top: 20px;
+    display: block;
+  }
 `;
 
 export const Testimonials = () => (
   <Wrapper id="testimonials">
     <HeroUnit title="lorem ipsum" subtitle={["testimonials"]} />
-    <TestimonialsList>
-      <TestimonialsListItem>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. In laoreet
-          felis odio. Suspendisse eu sollicitudin tortor. Nullam fringilla
-          sagittis quam eu maximus. Mauris sollicitudin aliquam egestas.
-        </p>
-        <strong>Paul T.</strong>
-      </TestimonialsListItem>
-    </TestimonialsList>
+    <TestimonialsListWrapper>
+      <button>
+        <MdKeyboardArrowLeft size="60px" />
+      </button>
+      <TestimonialsList>
+        <TestimonialsListItem>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. In laoreet
+            felis odio. Suspendisse eu sollicitudin tortor. Nullam fringilla
+            sagittis quam eu maximus. Mauris sollicitudin aliquam egestas.
+          </p>
+          <strong>Paul T.</strong>
+        </TestimonialsListItem>
+      </TestimonialsList>
+      <button>
+        <MdKeyboardArrowRight size="60px" />
+      </button>
+    </TestimonialsListWrapper>
   </Wrapper>
 );
